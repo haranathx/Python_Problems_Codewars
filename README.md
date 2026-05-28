23afff8f96853b833155ad7508db14545254bec4
haranathx <haranathx@gmail.com>
2026-05-28 15:26:40 +0530

    CW

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+#git clone git@HaranathX:haranathx/repo.git
+
+
 # -Python_Problems_Codewars
 Education Purposes Only
---
+++ prob1.py
@@ -0,0 +1,13 @@
+# Write a function `greet` that returns "hello world!"
+
+def greet():
+    message = "hello world!"
+    print(message)
+    return message
+
+# How to call it:
+greet()  # Output: hello world!
+
+
+# def greet():
+#     return "hello world!"
